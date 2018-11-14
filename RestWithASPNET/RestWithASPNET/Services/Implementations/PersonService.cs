a6ee9b34219e2c72174ff29a2803a72bca8d288e
Megaxlr3 <carlos.sccl@gmail.com>
2018-11-14 14:56:21 -0200

    implement crud operators

--- RestWithASPNET/RestWithASPNET/Services/Implementations/PersonService.cs
+++ RestWithASPNET/RestWithASPNET/Services/Implementations/PersonService.cs
@@ -1,44 +1,57 @@
 ﻿using RestWithASPNET.Models;
+using RestWithASPNET.Models.Context;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace RestWithASPNET.Services.Implementations
 {
     public class PersonService : IPersonService
     {
+        private MySQLContext _context;
+
+        public PersonService(MySQLContext context)
+        {
+            _context = context;
+        }
+
         public Person Create(Person person)
         {
+            try
+            {
+                _context.Persons.Add(person);
+                _context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+
             return person;
         }
 
         public void Delete(long id)
         {
+            var result = _context.Persons.SingleOrDefault(p => p.Id.Equals(id));
+            
+            try
+            {
+                if (result != null)
+                {
+                    _context.Persons.Remove(result);
+                    _context.SaveChanges();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
         }
 
         public List<Person> FindAll()
         {
-            List<Person> persons = new List<Person>();
-
-            for (int i = 0; i < 8; i++)
-            {
-                Person person = MockPerson(i);
-
-                persons.Add(person);
-            }
-
-            return persons;
-        }
-
-        private Person MockPerson(int i)
-        {
-            return new Person
-            {
-                Id = i,
-                FirstName = "FName " + i.ToString(),
-                LastName = "LName " + i.ToString(),
-                Address = "Address " + i.ToString(),
-                Gender = "Gender " + i.ToString()
-            };
+            return _context.Persons.ToList();
         }
 
         public Person FindById(long id)
@@ -55,7 +68,27 @@ namespace RestWithASPNET.Services.Implementations
 
         public Person Update(Person person)
         {
+            if (!Exist(person.Id)) return new Person();
+
+            var result = _context.Persons.SingleOrDefault(p => p.Id.Equals(person.Id));
+
+            try
+            {
+                _context.Entry(person).CurrentValues.SetValues(person);
+                _context.SaveChanges();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
             return person;
+        }
+
+        private bool Exist(long? id)
+        {
+            return _context.Persons.Any(p => p.Id.Equals(id));
         }
     }
 }
